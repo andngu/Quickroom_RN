7d9a1af565699f8e58386c7fa047ff7a37d731ac
andngu <34722986+andngu@users.noreply.github.com>
2018-05-02 12:23:19 -0500

    render buildings with flatlist

--- src/components/screens/BuildingScreen.js
+++ src/components/screens/BuildingScreen.js
@@ -1,49 +1,66 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { FlatList } from 'react-native';
 import { observer, inject } from 'mobx-react';
-import BuildingList from '../BuildingList';
+import { Card } from '../common';
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    backgroundColor: '#ddd',
+  },
+  touchableCard: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    height: 150,
+    width: 375,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buildingName: {
+    fontSize: 35,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 @inject('BuildingStore')
 @observer
 class BuildingScreen extends Component {
-  render() {
-    const { BuildingStore } = this.props;
+ 
+  renderItem = ({ item, index }) => {
     return (
-      <ScrollView>
-        <BuildingList
-          building={BuildingStore.getBizzell()}
-          onPress={() => {
-            this.props.navigation.navigate('Room');
-            BuildingStore.setBizzell();
-          }}
-        />
-
-        <BuildingList
-          building={BuildingStore.getDevon()}
-          onPress={() => {
-            this.props.navigation.navigate('Room');
-            BuildingStore.setDevon();
-          }}
-        />
-
-        <BuildingList
-          building={BuildingStore.getInnovation()}
-          onPress={() => {
-            this.props.navigation.navigate('Room');
-            BuildingStore.setInnovation();
-          }}
-        />
-
-        <BuildingList
-          building={BuildingStore.getWagner()}
-          onPress={() => {
-            this.props.navigation.navigate('Room');
-            BuildingStore.setWagner();
-          }}
-        />
-      </ScrollView>
+      <Card>
+    <TouchableOpacity 
+      style={styles.touchableCard} 
+      onPress={() => {
+        BuildingStore.selectBuilding(item.title);
+        this.props.navigation.navigate('Room');
+      }}>
+      <ImageBackground source={{ uri: item.image }} style={styles.image}>
+        <Text style={styles.buildingName}>{item.title}</Text>
+      </ImageBackground>
+    </TouchableOpacity>
+      </Card>
     );
   }
+
+  render() {
+    const data = BuildingStore.buildings.slice() // don't forget copy the list from store
+
+    return (
+      <FlatList
+        data={data}
+        extraData={data}
+        keyExtractor={(item, index) => `${index}`}
+        renderItem={this.renderItem}
+      />
+    )
+  }
+}
 }
 
 export default BuildingScreen;
